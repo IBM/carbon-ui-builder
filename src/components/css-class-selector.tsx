9234fed91062cfaec6f6b2353bfb3e85632593b7
Zvonimir Fras <zvonimir.fras@ibm.com>
2021-12-21 09:33:08 -0500

    Add css class selector component

---
+++ src/components/css-class-selector.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import {
+	FormLabel,
+	Tag
+} from 'carbon-components-react';
+
+const compareClasses = (sc1: any, sc2: any) => sc1.name < sc2.name ? -1 : 1;
+
+export const CssClassSelector = ({ styleClasses, selectedClasses, setSelectedClasses }: any) => {
+	const _getAvailableClasses = () => {
+		// available is anything in styleClasses, not yet in selecteClasses, sorted
+		return styleClasses
+			.filter((sc: any) => !selectedClasses.find((ssc: any) => ssc.id === sc.id))
+			.sort(compareClasses);
+	};
+
+	const [availableClasses, setAvailableClasses] = useState(_getAvailableClasses());
+
+	useEffect(() => {
+		if (!selectedClasses) {
+			setSelectedClasses([]);
+		}
+	}, [selectedClasses, setSelectedClasses]);
+
+	useEffect(() => {
+		// update available classes based on styleClasses and selectedClasses
+		setAvailableClasses(_getAvailableClasses());
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [styleClasses, selectedClasses]);
+
+	useEffect(() => {
+		// update the contents of selected classes when needed
+		setSelectedClasses(styleClasses.filter((sc: any) => !!selectedClasses.find((ssc: any) => ssc.id === sc.id)));
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [styleClasses]);
+
+	const selectStyleClass = (styleClass: any) => {
+		setSelectedClasses([...selectedClasses, styleClass]);
+		setAvailableClasses(availableClasses.filter((c: any) => c.id !== styleClass.id))
+	};
+
+	const deselectStyleClass = (styleClass: any) => {
+		setSelectedClasses(selectedClasses.filter((c: any) => c.id !== styleClass.id));
+		setAvailableClasses([...availableClasses, styleClass].sort(compareClasses));
+	};
+
+	return (
+		<div style={{marginTop: '1rem'}}>
+			<p>Custom CSS classes</p>
+			<FormLabel>Selected classes</FormLabel>
+			<br />
+			{
+				selectedClasses?.map((styleClass: any) => (
+					<Tag
+					filter
+					onClose={() => deselectStyleClass(styleClass)}>
+						{styleClass.name}
+					</Tag>
+				))
+			}
+			<br />
+			<FormLabel>Available classes</FormLabel>
+			<br />
+			{
+				availableClasses.map((styleClass: any) => (
+					<Tag
+					onClick={() => selectStyleClass(styleClass)}>
+						{styleClass.name}
+					</Tag>
+				))
+			}
+		</div>
+	);
+};
