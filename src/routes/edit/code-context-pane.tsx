a01098e938b9de5e1df633f85a067cf7bebf45c8
Zvonimir Fras <zvonimir.fras@ibm.com>
2022-01-21 09:47:09 -0500

    Refactor code context ui a bit
    related to #21

--- src/routes/edit/code-context-pane.tsx
+++ src/routes/edit/code-context-pane.tsx
@@ -2,27 +2,21 @@ import React from 'react';
 
 import {
 	AComponentCodeUI,
-	ATextInputCodeUI,
-	ATextAreaCodeUI,
-	ACheckboxCodeUI
+	allComponents
 } from '../../fragment-components';
 
 import { getSelectedComponent, updatedState } from '../../components/fragment';
 
 const showComponentCodeOptions = (selectedComponent: any, setComponent: any) => {
-	switch (selectedComponent.type) {
-		case 'textinput':
-			return <ATextInputCodeUI selectedComponent={selectedComponent} setComponent={setComponent} />;
-
-		case 'textarea':
-			return <ATextAreaCodeUI selectedComponent={selectedComponent} setComponent={setComponent} />;
-
-		case 'checkbox':
-			return <ACheckboxCodeUI selectedComponent={selectedComponent} setComponent={setComponent} />;
-
-		default:
-			return <AComponentCodeUI selectedComponent={selectedComponent} setComponent={setComponent} />;
+	for (let [key, component] of Object.entries(allComponents)) {
+		// Find the UI for editing code props for our component
+		if (selectedComponent.type === key && component.componentInfo.codeUI) {
+			return <component.componentInfo.codeUI
+				selectedComponent={selectedComponent}
+				setComponent={setComponent} />
+		}
 	}
+	return <AComponentCodeUI selectedComponent={selectedComponent} setComponent={setComponent} />;
 };
 
 export const CodeContextPane = ({fragment, setFragment}: any) => {
