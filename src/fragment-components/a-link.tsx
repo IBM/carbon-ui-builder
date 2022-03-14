cd44116e9fa3e1ecfd6482cd572e505e7a0e9a73
Moiz Masud <moiz.masud@ibm.com>
2022-03-14 13:11:06 -0400

    added href

--- src/fragment-components/a-link.tsx
+++ src/fragment-components/a-link.tsx
@@ -45,6 +45,24 @@ export const ALinkStyleUI = ({selectedComponent, setComponent}: any) => {
 	</>
 };
 
+export const ALinkCodeUI = ({selectedComponent, setComponent}: any) => {
+	return <>
+		<TextInput
+			value={selectedComponent.codeContext?.href || ''}
+			labelText='href'
+			onChange={(event: any) => {
+				setComponent({
+					...selectedComponent,
+					codeContext: {
+						...selectedComponent.codeContext,
+						href: event.currentTarget.value
+					}
+				});
+			}}
+		/>
+	</>
+};
+
 export const ALink = ({
 	children,
 	componentObj,
@@ -58,6 +76,7 @@ export const ALink = ({
 			<Link
 			inline={componentObj.inline}
 			disabled={componentObj.disabled}
+			href={componentObj.codeContext?.href}
 			className={componentObj.cssClasses?.map((cc: any) => cc.id).join(' ')}>
 				{children}
 			</Link>
@@ -68,6 +87,7 @@ export const ALink = ({
 export const componentInfo: ComponentInfo = {
 	component: ALink,
 	styleUI: ALinkStyleUI,
+	codeUI: ALinkCodeUI,
 	render: ({ componentObj, select, remove, selected }) => <ALink
 		componentObj={componentObj}
 		select={select}
@@ -79,20 +99,24 @@ export const componentInfo: ComponentInfo = {
 	name: 'Link',
 	defaultComponentObj: {
 		type: 'link',
-		text: 'Link'
+		text: 'Link',
+		inline: false,
+		disabled: false
 	},
 	image,
 	codeExport: {
 		angular: {
 			inputs: ({json}) => `
-			@Input() ${nameStringToVariableString(json.codeContext?.name)}Inline = ${json.inline};
-			@Input() ${nameStringToVariableString(json.codeContext?.name)}Disabled = ${json.disabled};`,
+			@Input() ${nameStringToVariableString(json.codeContext?.name)}Inline = ${json.inline}'
+			@Input() ${nameStringToVariableString(json.codeContext?.name)}Disabled = ${json.disabled};
+			@Input() ${nameStringToVariableString(json.codeContext?.name)}Href = '${json.href}'`,
 			outputs: ({json}) => `@Output() ${nameStringToVariableString(json.codeContext?.name)}Clicked = new EventEmitter();`,
 			imports: ['LinkModule'],
 			code: ({json}) => {
 				return `<a
 				[inline]="${nameStringToVariableString(json.codeContext?.name)}Inline"
 				[disabled]="${nameStringToVariableString(json.codeContext?.name)}Disabled"
+				[href]=${nameStringToVariableString(json.codeContext?.name)}Href
 					${angularClassNamesFromComponentObj(json)}>
 						${json.text}
 				</a>`;
@@ -104,6 +128,7 @@ export const componentInfo: ComponentInfo = {
 				return `<Link
 				${json.disabled !== undefined && json.disabled !== '' ? `disabled="${json.disabled}"` : ''}
 				${json.inline !== undefined && json.inline !== '' ? `inline="${json.inline}"` : ''}
+				${json.codeContext?.href !== undefined && json.codeContext?.href !== '' ? `href='${json.codeContext?.href}'` : ''}
 				${reactClassNamesFromComponentObj(json)}>
 					${json.text}
 				</Link>`;
