46b87571d3198d66f927d964293b3a94073aeb61
Akshat Patel <akshat@live.ca>
2022-04-14 12:40:02 -0400

    Use draggable component list
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- src/fragment-components/a-progress-indicator.tsx
+++ src/fragment-components/a-progress-indicator.tsx
@@ -1,36 +1,23 @@
 import React from 'react';
 import {
-	Button,
 	Checkbox,
 	ProgressIndicator,
 	ProgressStep,
-	TextInput,
-	Tile
+	TextInput
 } from 'carbon-components-react';
-import {
-	Add32,
-	Draggable16,
-	TrashCan32
-} from '@carbon/icons-react';
 import { AComponent } from './a-component';
 import { css } from 'emotion';
 import { ComponentCssClassSelector } from '../components/css-class-selector';
 import { ComponentInfo } from '.';
 import image from './../assets/component-icons/progress-indicator.svg';
+import { DraggableTileList } from '../components';
 import {
 	angularClassNamesFromComponentObj,
 	nameStringToVariableString,
 	reactClassNamesFromComponentObj
 } from '../utils/fragment-tools';
 
-const onDragStyle = css`
-	outline: 2px solid #1666fe;
-	outline-offset; -2px;
-`;
-
 export const AProgressIndicatorStyleUI = ({ selectedComponent, setComponent }: any) => {
-	const tiles = [...selectedComponent.progressSteps];
-
 	const handleStepUpdate = (key: string, value: any, index: number) => {
 		const step = {
 			...selectedComponent.progressSteps[index],
@@ -47,59 +34,40 @@ export const AProgressIndicatorStyleUI = ({ selectedComponent, setComponent }: a
 		});
 	};
 
-	const addToStepsList = (event: any) => {
-		event.stopPropagation();
+	const template = (handleStepUpdate: any, item: any, index: number) => {
+		return <>
+			<TextInput
+				light
+				size="sm"
+				value={item.label}
+				labelText="Label"
+				onChange={(event: any) => handleStepUpdate('label', event.currentTarget.value, index)} />
+			<TextInput
+				light
+				value={item.secondaryLabel}
+				labelText='Secondary label'
+				onChange={(event: any) => handleStepUpdate('secondaryLabel', event.currentTarget.value, index)} />
+			<div style={{ display: 'flex' }}>
+				<Checkbox
+					style={{ display: 'inline-flex' }}
+					labelText='Is invalid'
+					id={`invalid-select-${index}`}
+					checked={item.invalid}
+					onChange={(checked: any) => handleStepUpdate('invalid', checked, index)} />
+				<Checkbox
+					style={{ display: 'inline-flex' }}
+					labelText='Is disabled'
+					id={`disabled-select-${index}`}
+					checked={item.disabled}
+					onChange={(checked: any) => handleStepUpdate('disabled', checked, index)} />
+			</div>
+		</>;
+	};
+
+	const updateStepList = (newList: any[]) => {
 		setComponent({
 			...selectedComponent,
-			progressSteps: [
-				...selectedComponent.progressSteps,
-				{
-					id: new Date().getTime().toString(),
-					label: 'Step',
-					description: 'Description',
-					secondaryLabel: 'Optional label',
-					invalid: false,
-					disabled: false
-				}
-			]
-		});
-	};
-
-	const removeFromStepsList = (event: any, id: number) => {
-		event.stopPropagation();
-		setComponent({
-			...selectedComponent,
-			progressSteps: selectedComponent.progressSteps.filter((step: any) => step.id !== id)
-		});
-	};
-
-	const dragStart = (event: any) => {
-		event.dataTransfer.setData('id', event.target.id);
-	};
-
-	const onDragOver = (event: any) => {
-		event.preventDefault();
-		event.currentTarget.classList.add(onDragStyle);
-	};
-
-	const onDragLeave = (event: any) => {
-		event.currentTarget.classList.remove(onDragStyle);
-	};
-
-	const onDrop = (event: any, index: number) => {
-		event.currentTarget.classList.remove(onDragStyle);
-		const stepId = event.dataTransfer.getData('id');
-		const previousIndex = selectedComponent.progressSteps.findIndex((step: any) => step.id === stepId);
-		const tile = { ...selectedComponent.progressSteps[previousIndex] };
-		tiles.splice(previousIndex, 1);
-
-		setComponent({
-			...selectedComponent,
-			progressSteps: [
-				...tiles.slice(0, index),
-				tile,
-				...tiles.slice(index)
-			]
+			progressSteps: newList
 		});
 	};
 
@@ -115,83 +83,36 @@ export const AProgressIndicatorStyleUI = ({ selectedComponent, setComponent }: a
 				});
 			}}
 		/>
-		<div>
-			{
-				tiles.map((step: any, index: number) => (
-					<Tile
-						key={step.id}
-						id={step.id}
-						draggable={true}
-						onDragStart={dragStart}
-						onDragLeave={onDragLeave}
-						onDragOver={onDragOver}
-						onDrop={(event: any) => onDrop(event, index)}
-						style={{ marginBottom: '1rem', position: 'relative' }}>
-						<Draggable16 style={{ position: 'absolute', top: '50%', left: 0 }} />
-						<div style={{ display: 'inline-flex', alignItems: 'flex-end', width: '100%' }}>
-							<TextInput
-								light
-								size="sm"
-								value={step.label}
-								labelText="Label"
-								onChange={(event: any) => handleStepUpdate('label', event.currentTarget.value, index)} />
-							<Button
-								style={{ marginLeft: 12 }}
-								size="sm"
-								kind="danger--tertiary"
-								iconDescription="Delete step"
-								hasIconOnly
-								renderIcon={TrashCan32}
-								onClick={(event: any) => removeFromStepsList(event, step.id)} />
-						</div>
-						<TextInput
-							light
-							value={step.secondaryLabel}
-							labelText='Secondary label'
-							onChange={(event: any) => handleStepUpdate('secondaryLabel', event.currentTarget.value, index)} />
-						<div style={{ display: 'flex' }}>
-							<Checkbox
-								style={{ display: 'inline-flex' }}
-								labelText='Is invalid'
-								id={`invalid-select-${step.id}`}
-								checked={step.invalid}
-								onChange={(checked: any) => handleStepUpdate('invalid', checked, index)} />
-							<Checkbox
-								style={{ display: 'inline-flex' }}
-								labelText='Is disabled'
-								id={`disabled-select-${step.id}`}
-								checked={step.disabled}
-								onChange={(checked: any) => handleStepUpdate('disabled', checked, index)} />
-						</div>
-					</Tile>
-				))
-			}
-			<Button
-				renderIcon={Add32}
-				kind="tertiary"
-				size="sm"
-				onClick={addToStepsList}>
-					Add step
-			</Button>
-		</div>
+		<DraggableTileList
+			data={[...selectedComponent.progressSteps]}
+			setData={updateStepList}
+			handleStepUpdate={handleStepUpdate}
+			defaultObject={{
+				label: 'Step',
+				description: 'Description',
+				secondaryLabel: 'Optional label',
+				invalid: false,
+				disabled: false
+			}}
+			template={template} />
 		<ComponentCssClassSelector componentObj={selectedComponent} setComponent={setComponent} />
 	</>;
 };
 
 export const AProgressIndicatorCodeUI = ({ selectedComponent, setComponent }: any) => {
 	return <TextInput
-			value={selectedComponent.codeContext?.name}
-			labelText='Input name'
-			onChange={(event: any) => {
-				setComponent({
-					...selectedComponent,
-					codeContext: {
-						...selectedComponent.codeContext,
-						name: event.currentTarget.value
-					}
-				});
-			}}
-		/>;
+		value={selectedComponent.codeContext?.name}
+		labelText='Input name'
+		onChange={(event: any) => {
+			setComponent({
+				...selectedComponent,
+				codeContext: {
+					...selectedComponent.codeContext,
+					name: event.currentTarget.value
+				}
+			});
+		}}
+	/>;
 };
 
 export const AProgressIndicator = ({
@@ -209,8 +130,8 @@ export const AProgressIndicator = ({
 			vertical={componentObj.isVertical}
 			className={componentObj.cssClasses?.map((cc: any) => cc.id).join(' ')}>
 				{
-					componentObj.progressSteps.map((step: any) => (
-						<ProgressStep {...step} key={step.id} id={`step-${step.id}`} />
+					componentObj.progressSteps.map((step: any, index: number) => (
+						<ProgressStep {...step} key={index} />
 					))
 				}
 			</ProgressIndicator>
@@ -231,7 +152,6 @@ export const componentInfo: ComponentInfo = {
 		spacing: true,
 		progressSteps: [
 			{
-				id: new Date().getTime().toString(),
 				label: 'Step 0',
 				description: 'Step 0: Getting started with carbon design system',
 				secondaryLabel: 'Optional Label',
