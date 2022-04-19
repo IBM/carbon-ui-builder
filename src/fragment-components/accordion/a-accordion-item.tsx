71658923ed196638c209b7b3a0c8eb161e94e23d
Moiz Masud <moizkhan_007@hotmail.com>
2022-04-19 13:11:39 -0400

    Update src/fragment-components/accordion/a-accordion-item.tsx
    Co-authored-by: Zvonimir Fras <zvonimir.fras@gmail.com>

--- src/fragment-components/accordion/a-accordion-item.tsx
+++ src/fragment-components/accordion/a-accordion-item.tsx
@@ -105,7 +105,7 @@ export const componentInfo: ComponentInfo = {
 			{componentObj.items.map((child: any) => renderComponents(child))}
 	</AAccordionItem>,
 	keywords: ['accordion', 'item'],
-	name: 'Accordion Item',
+	name: 'Accordion item',
 	type: 'accordion-item',
 	defaultComponentObj: {
 		type: 'accordionitem',
