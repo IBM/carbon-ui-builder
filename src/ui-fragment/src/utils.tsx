f2bd186823d362dc249379dbb7c44ac00a0fda50
Zvonimir Fras <zvonimir.fras@ibm.com>
2022-07-19 12:21:24 -0400

    Add UIGrid to fragment preview

---
+++ src/ui-fragment/src/utils.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { UIButton } from './components/ui-button';
+import { UIColumn } from './components/ui-column';
+import { UIGrid } from './components/ui-grid';
+import { UIRow } from './components/ui-row';
+
+export const setItemInState = (item: any, state: any, setState: (state: any) => void) => {
+	const itemIndex = state.items.indexOf(item);
+	setState({
+		...state,
+		items: [
+			...state.items.slice(0, itemIndex),
+			item,
+			...state.items.slice(itemIndex + 1)
+		]
+	});
+};
+
+export const renderComponents = (state: any, setState: (state: any) => void, setGlobalState: (state: any) => void) => {
+	switch (state.type) {
+		case 'button':
+			return <UIButton key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
+
+		case 'grid':
+			return <UIGrid key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
+
+		case 'row':
+			return <UIRow key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
+
+		case 'column':
+			return <UIColumn key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
+
+		default:
+			break;
+	}
+
+	if (state.items && Array.isArray(state.items)) {
+		// setItem is a setState for that particular item
+		const setItem = (item: any) => setItemInState(item, state, setState);
+
+		return state.items.map((item: any) => renderComponents(item, setItem, setState));
+	}
+};
