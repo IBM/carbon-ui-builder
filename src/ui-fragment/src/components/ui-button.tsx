f841271091e73c82b91aafaa6b155dded4866ef6
Zvonimir Fras <zvonimir.fras@ibm.com>
2022-07-19 12:20:19 -0400

    Add styles to UIButton

--- src/ui-fragment/src/components/ui-button.tsx
+++ src/ui-fragment/src/components/ui-button.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from 'carbon-components-react';
+import { CssClasses } from '../types';
 
 export interface ButtonState {
 	type: string;
 	kind: string;
 	text: string;
 	id: string | number;
+	cssClasses?: CssClasses[];
 	codeContext?: {
 		name: string;
 	};
@@ -21,5 +23,5 @@ export const UIButton = ({ state }: {
 		return <></>;
 	}
 
-	return <Button kind={state.kind}>{state.text}</Button>;
+	return <Button kind={state.kind} className={state.cssClasses?.map((cc: any) => cc.id).join(' ')}>{state.text}</Button>;
 };
