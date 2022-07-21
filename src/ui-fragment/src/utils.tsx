6de59572f08788762e74b3f7500deae8d3f99fb3
Zvonimir Fras <zvonimir.fras@ibm.com>
2022-07-21 10:17:58 -0400

    Add UINumberInput to fragment preview

--- src/ui-fragment/src/utils.tsx
+++ src/ui-fragment/src/utils.tsx
@@ -12,6 +12,7 @@ import { UIDropdown } from './components/ui-dropdown';
 import { UIGrid } from './components/ui-grid';
 import { UILink } from './components/ui-link';
 import { UILoading } from './components/ui-loading';
+import { UINumberInput } from './components/ui-number-input';
 import { UIRadio } from './components/ui-radio';
 import { UIRadioGroup } from './components/ui-radio-group';
 import { UIRow } from './components/ui-row';
@@ -79,6 +80,9 @@ export const renderComponents = (state: any, setState: (state: any) => void, set
 
 		case 'link':
 			return <UILink key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
+
+		case 'number-input':
+			return <UINumberInput key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
 
 		case 'text':
 			return <UIText key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
