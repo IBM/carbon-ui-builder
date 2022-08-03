bb1833f95e959d170651283bab4dea34e74413a0
Moiz Masud <moiz.masud@ibm.com>
2022-08-03 15:12:49 -0400

    Add align for angular export

--- src/fragment-components/a-tooltip.tsx
+++ src/fragment-components/a-tooltip.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import {
 	Tooltip,
-	Checkbox,
 	TextInput,
-	Dropdown,
-	DefinitionTooltip
+	Dropdown
 } from 'carbon-components-react';
 import { AComponent, ComponentInfo } from './a-component';
 import image from './../assets/component-icons/link.svg';
@@ -13,61 +11,44 @@ import {
 	nameStringToVariableString,
 	reactClassNamesFromComponentObj
 } from '../utils/fragment-tools';
+import { css } from 'emotion';
+
+const preventCheckEvent = css`
+.bx--tooltip__label {
+	pointer-events: none;
+}`;
 
 export const ATooltipSettingsUI = ({ selectedComponent, setComponent }: any) => {
 	const alignItems = [
-		{ id: 'top', text: 'Top' },
 		{ id: 'bottom', text: 'Bottom' },
-		{ id: 'left', text: 'Left' },
-		{ id: 'right', text: 'Right' }
-	];
-
-	const definitionTooltipAlign = [
 		{ id: 'top', text: 'Top' },
-		{ id: 'bottom', text: 'Bottom' }
+		{ id: 'right', text: 'Right' },
+		{ id: 'left', text: 'Left' }
 	];
 
 	return <>
 		<Dropdown
 			label='Align'
 			titleText='Align'
-			items={selectedComponent.definitionTooltip ? definitionTooltipAlign : alignItems}
+			items={alignItems}
 			selectedItem={alignItems.find(item => item.id === selectedComponent.align)}
 			itemToString={(item: any) => (item ? item.text : '')}
 			onChange={(event: any) => setComponent({
 				...selectedComponent,
 				align: event.selectedItem.id
 			})} />
-		<Checkbox
-			labelText='Definition tooltip'
-			id='definition-tooltip'
-			checked={selectedComponent.definitionTooltip}
-			onChange={(checked: boolean) => setComponent({
-				...selectedComponent,
-				definitionTooltip: checked
-		})} />
 		<TextInput
 			value={selectedComponent.description}
 			labelText='Description'
 			onChange={(event: any) => setComponent({
 				...selectedComponent,
 				description: event.currentTarget.value
-			})} />
-		{
-			selectedComponent.definitionTooltip && <TextInput
-				value={selectedComponent.definitionTooltipText}
-				labelText='Trigger text'
-				onChange={(event: any) => setComponent({
-					...selectedComponent,
-					definitionTooltipText: event.currentTarget.value
-			})} />
-		}
+		})} />
 	</>;
 };
 
 export const ATooltipCodeUI = ({ selectedComponent, setComponent }: any) => {
-	return <>
-		<TextInput
+	return <TextInput
 			value={selectedComponent.codeContext?.name}
 			labelText='Input name'
 			onChange={(event: any) => {
@@ -79,12 +60,10 @@ export const ATooltipCodeUI = ({ selectedComponent, setComponent }: any) => {
 					}
 				});
 			}}
-		/>
-	</>;
+		/>;
 };
 
 export const ATooltip = ({
-	children,
 	componentObj,
 	...rest
 }: any) => {
@@ -92,22 +71,14 @@ export const ATooltip = ({
 		<AComponent
 		componentObj={componentObj}
 		rejectDrop={true}
+		className={`${preventCheckEvent} ${componentObj.cssClasses?.map((cc: any) => cc.id).join(' ')} `}
 		{...rest}>
-		{
-			componentObj.definitionTooltip ?
-			<DefinitionTooltip
-			align={componentObj.align}
-			className={componentObj.cssClasses?.map((cc: any) => cc.id).join(' ')}
-			definition={componentObj.description}>
-				{componentObj.definitionTooltipText}
-		  	</DefinitionTooltip> :
 			<Tooltip
 			label={componentObj.description}
 			align={componentObj.align}
 			className={componentObj.cssClasses?.map((cc: any) => cc.id).join(' ')}>
 				{componentObj.description}
 			</Tooltip>
-		}
 		</AComponent>
 	);
 };
@@ -121,41 +92,40 @@ export const componentInfo: ComponentInfo = {
 	type: 'tooltip',
 	defaultComponentObj: {
 		type: 'tooltip',
-		align: 'top',
-		definitionTooltip: false,
-		definitionTooltipText: 'Definition Tooltip',
+		align: 'bottom',
 		description: 'This is some tooltip text'
 	},
 	image,
 	codeExport: {
 		angular: {
-			inputs: ({ json }) => ``,
-			outputs: ({ json }) => ``,
-			imports: ['DialogModule, PlaceholderModule, TagModule'],
+			inputs: ({ json }) => `@Input() ${nameStringToVariableString(json.codeContext?.name)}Description = "${json.description}";`,
+			outputs: (_) => '',
+			imports: ['DialogModule, PlaceholderModule, TagModule, IconModule'],
 			code: ({ json }) => {
-				return `${json.definitionTooltip ?
-					`<ibm-tooltip-definition
-						[content]="${json.description}"
-						[placement]="${json.align}"
-						{{${json.definitionTooltipText}}}
-					</ibm-tooltip-definition>` :
-					`<div class="bx--tooltip__label">
+				return `
+				<div class="bx--tooltip__label">
 					<span
-						[ibmTooltip]="${json.description}"
+						${angularClassNamesFromComponentObj(json)}
+						[ibmTooltip]="${nameStringToVariableString(json.codeContext?.name)}Description"
 						trigger="click"
-						[placement]="${json.align}">
+						[placement]="'${json.align}'" >
 						<div role="button">
 							<svg ibmIcon="information--filled" size="16"></svg>
 						</div>
 					</span>
 				</div>
-				<ibm-placeholder></ibm-placeholder>`}`;
+				<ibm-placeholder></ibm-placeholder>`;
 			}
 		},
 		react: {
-			imports: [''],
+			imports: ['Tooltip'],
 			code: ({ json }) => {
-				return ``;
+				return `<Tooltip
+					${reactClassNamesFromComponentObj(json)}
+					label="${json.description}"
+					align="${json.align}">
+						${json.description}
+					</Tooltip>`;
 			}
 		}
 	}
