679ba725a77a3a1c1a20ff78d69c40d2a887e0d3
Zvonimir Fras <zvonimir.fras@ibm.com>
2022-08-04 09:52:20 -0400

    Add UIOverflowMenu to fragment preview

--- src/ui-fragment/src/utils.tsx
+++ src/ui-fragment/src/utils.tsx
@@ -13,6 +13,8 @@ import { UIGrid } from './components/ui-grid';
 import { UILink } from './components/ui-link';
 import { UILoading } from './components/ui-loading';
 import { UINumberInput } from './components/ui-number-input';
+import { UIOverflowMenu } from './components/ui-overflow-menu';
+import { UIOverflowMenuItem } from './components/ui-overflow-menu-item';
 import { UIProgressIndicator } from './components/ui-progress-indicator';
 import { UIRadio } from './components/ui-radio';
 import { UIRadioGroup } from './components/ui-radio-group';
@@ -106,6 +108,12 @@ export const renderComponents = (state: any, setState: (state: any) => void, set
 
 		case 'text-input':
 			return <UITextInput key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
+
+		case 'overflow-menu':
+			return <UIOverflowMenu key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
+
+		case 'overflow-menu-item':
+			return <UIOverflowMenuItem key={state.id} state={state} setState={setState} setGlobalState={setGlobalState} />;
 
 		default:
 			break;
