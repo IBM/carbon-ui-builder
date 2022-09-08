1164c0e36751b981aa7c701012667bd589876a2e
Zvonimir Fras <zvonimir.fras@ibm.com>
2022-09-08 11:21:55 -0400

    Use github navigator for launch

--- src/routes/launch/index.tsx
+++ src/routes/launch/index.tsx
@@ -1,48 +1,17 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { GithubContext } from '../../context';
-import { GithubFilePreview } from '../../components/github-file-preview';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { GithubNavigator } from '../../components/github-navigator';
 
 export const Launch = () => {
 	const params = useParams();
-	const { getContent } = useContext(GithubContext);
-	const [state, setState] = useState({
-		fragmentState: null as any,
-		folderContent: [] as any[]
-	} as any);
 
-	useEffect(() => {
-		if (!params.owner || !params.repo) {
-			return;
-		}
-
-		getContent(params.owner, params.repo, params['*'] || '').then((content: any) => setState(content));
-	}, [params, getContent]);
-
-	if (!params.owner || !params.repo) {
+	if (!params.owner) {
 		return <div>Nothing to see here. <a href='/'>Go home.</a></div>;
 	}
 
-	if (!state.folderContent?.length && !state.fragmentState && !state.fileContent) {
-		return <div>Loading...</div>;
-	}
-
-	if (state.folderContent && state.folderContent.length) {
-		return <ul>
-			{
-				state.folderContent.map((item: any) => <li key={item.name}>
-					<Link
-					className='bx--btn bx--btn--ghost bx--btn--sm'
-					to={`/launch/${params.owner}/${params.repo}/${item.path}`}>
-						{item.name}{item.type === 'dir' ? '/' : ''}
-					</Link>
-				</li>)
-			}
-		</ul>;
-	}
-
-	return <GithubFilePreview
-		fragmentState={state.fragmentState}
-		fileContent={state.fileContent}
-		fileContentBase64={state.fileContentBase64} />;
+	return <GithubNavigator
+		basePath='/launch'
+		path={params['*']}
+		repoName={params.repo}
+		repoOrg={params.owner} />;
 };
