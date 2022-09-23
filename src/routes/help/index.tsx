294eab24e50fc080bb923c87dc0b6830cce403ed
Zvonimir Fras <zvonimir.fras@ibm.com>
2022-09-23 15:14:07 -0400

    Externalize markdown styles
    Signed-off-by: Moiz Masud <moizkhan_007@hotmail.com>

--- src/routes/help/index.tsx
+++ src/routes/help/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import { useParams } from 'react-router-dom';
@@ -12,48 +12,6 @@ const helpContainerStyle = css`
 	top: 3rem;
 	max-width: 100%;
 	margin: 1rem 3rem;
-
-	h1 {
-		margin-bottom: 2rem;
-	}
-
-	h2, h3 {
-		margin-top: 1rem;
-		margin-bottom: 1rem;
-	}
-
-	ul, ol {
-		margin-top: 1rem;
-		margin-bottom: 1rem;
-		margin-left: 2rem;
-		line-height: 1.5rem;
-	}
-
-	ul li {
-		list-style: initial;
-	}
-
-	ol li {
-		list-style: auto;
-	}
-
-	p {
-		line-height: 2rem;
-	}
-
-	table {
-		thead {
-			font-weight: bold;
-			font-size: 150%;
-			text-align: left;
-		}
-		tr, th, td {
-			padding 1rem;
-		}
-		tr {
-			border-bottom: 1px solid #888;
-		}
-	}
 `;
 
 const helpIndex: any = {
@@ -69,7 +27,7 @@ export const Help = () => {
 		axios.get(helpIndex[params.id || 'introduction']).then(response => setMarkdownContent(response.data));
 	}, [params.id]);
 
-	return <div className={helpContainerStyle}>
+	return <div className={cx(helpContainerStyle, 'markdown')}>
 		<ReactMarkdown remarkPlugins={[gfm]}>{markdownContent}</ReactMarkdown>
 	</div>;
 };
