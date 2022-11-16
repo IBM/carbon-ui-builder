9a0509cff736ac7a078db6a3a1cf1af589d62884
Moiz Masud <moizkhan_007@hotmail.com>
2022-11-15 23:57:28 -0500

    Add react export code
    Signed-off-by: Moiz Masud <moizkhan_007@hotmail.com>

--- src/fragment-components/a-select.tsx
+++ src/fragment-components/a-select.tsx
@@ -4,7 +4,8 @@ import {
 	Select,
 	SelectItem,
 	SelectItemGroup,
-	Checkbox
+	Checkbox,
+	Button
 } from 'carbon-components-react';
 import { AComponent, ComponentInfo } from './a-component';
 import image from './../assets/component-icons/link.svg';
@@ -13,10 +14,197 @@ import {
 	nameStringToVariableString,
 	reactClassNamesFromComponentObj
 } from '../utils/fragment-tools';
+import { useFragment } from '../context';
+import { DraggableTileList } from '../components/draggable-list';
+import { css } from 'emotion';
+import { updatedState } from '../components';
+
+const iconStyle = css`
+	min-height: 1rem;
+	float: right;
+	cursor: pointer`;
+
+const addButtonStyle = css`
+	width: 100%;
+	display: flex;
+	justify-content: center;`;
 
 export const ASelectSettingsUI = ({ selectedComponent, setComponent }: any) => {
-	return <>
+	const [fragment, setFragment] = useFragment();
 
+	// Adds new select item
+	const addOption = (offset = 0, itemsList: any) => setFragment({
+		...fragment,
+		data: updatedState(
+			fragment.data,
+			{
+				type: 'insert',
+				component: {
+					type: 'select-item',
+					text: 'New option',
+					value: 'new-option',
+					disabled: false,
+					hidden: false
+				}
+			},
+			itemsList.id,
+			itemsList.items.indexOf(itemsList) + offset
+		)
+	});
+
+	// Adds new category
+	const addNewCategory = (offset = 0) => setFragment({
+		...fragment,
+		data: updatedState(
+			fragment.data,
+			{
+				type: 'insert',
+				component: {
+					label: 'New category',
+					disabled: false,
+					items: [{
+						type: 'select-item',
+						text: 'New option',
+						value: 'new-option',
+						disabled: false,
+						hidden: false
+					}]
+				}
+			},
+			selectedComponent.id,
+			selectedComponent.items.indexOf(selectedComponent) + offset
+		)
+	});
+
+	const updateStepList = (newList: any[]) => {
+		setComponent({
+			...selectedComponent,
+			items: newList
+		});
+	};
+
+	const updateListItems = (key: string, value: any, index: number, list: any) => {
+		const step = {
+			...list.items[index],
+			[key]: value
+		};
+
+		setComponent({
+			...list,
+			items: [
+				...list.items.slice(0, index),
+				step,
+				...list.items.slice(index + 1)
+			]
+		});
+	};
+
+	const template = (selectItem: any, index: number) => <>
+		{
+			selectItem.items && selectItem.items.length > 0
+				? <>
+			<TextInput
+				light
+				value={selectItem.label}
+				labelText='Category label'
+				onChange={(event: any) => {
+					updateListItems('label', event.currentTarget.value, index, selectedComponent);
+				}} />
+			<Checkbox
+				labelText='Disabled'
+				id={`disabled-${index}`}
+				checked={selectItem.disabled}
+				onChange={(checked: boolean) => updateListItems('disabled', checked, index, selectedComponent)} />
+			<Button
+			size="sm"
+			kind="ghost"
+			className={iconStyle}
+			onClick={(event: any) => {
+				event.stopPropagation();
+				addOption(0, selectItem);
+			}}>Add item</Button>
+			{
+				selectItem.items.map((child: any, childIndex: any) => <>
+					<TextInput
+						light
+						value={child.text}
+						key={`child-text-${index}-${childIndex}`}
+						labelText='Option value'
+						onChange={(event: any) => updateListItems('text', event.currentTarget.value, childIndex, selectItem)} />
+					<Checkbox
+						labelText='Disabled'
+						id={`child-disabled-${index}-${childIndex}`}
+						checked={child.disabled}
+						onChange={(checked: boolean) => updateListItems('disabled', checked, childIndex, selectItem)}/>
+					<Checkbox
+						labelText='Hidden'
+						id={`child-hidden-${index}-${childIndex}`}
+						checked={child.hidden}
+						onChange={(checked: boolean) => updateListItems('hidden', checked, childIndex, selectItem)}/>
+				</>)
+			} </>
+				: <>
+			<TextInput
+				light
+				value={selectItem.text}
+				labelText='Option value'
+				onChange={(event: any) => updateListItems('text', event.currentTarget.value, index, selectedComponent)} />
+			<Checkbox
+				labelText='Disabled'
+				id={`disabled-${index}`}
+				checked={selectItem.disabled}
+				onChange={(checked: boolean) => updateListItems('disabled', checked, index, selectedComponent)} />
+			<Checkbox
+				labelText='Hidden'
+				id={`hidden-${index}`}
+				checked={selectItem.hidden}
+				onChange={(checked: boolean) => updateListItems('hidden', checked, index, selectedComponent)} />
+				</>
+		}
+	</>;
+	return <>
+		<TextInput
+			value={selectedComponent.labelText}
+			labelText='Select value'
+			onChange={(event: any) => setComponent({ ...selectedComponent, labelText: event.currentTarget.value })} />
+		<TextInput
+			value={selectedComponent.invalidText}
+			labelText='Invalid text value'
+			onChange={(event: any) => setComponent({ ...selectedComponent, invalidText: event.currentTarget.value })} />
+		<Checkbox
+			labelText='Disabled'
+			id='disable-label'
+			checked={selectedComponent.disabled}
+			onChange={(checked: boolean) => setComponent({ ...selectedComponent, disabled: checked })} />
+		<Checkbox
+			labelText='Inline'
+			id='inline'
+			checked={selectedComponent.inline}
+			onChange={(checked: boolean) => setComponent({ ...selectedComponent, inline: checked })} />
+		<Checkbox
+			labelText='Invalid'
+			id='invalid'
+			checked={selectedComponent.invalid}
+			onChange={(checked: boolean) => setComponent({ ...selectedComponent, invalid: checked })} />
+		<Button
+		className={addButtonStyle}
+		size="sm"
+		kind="ghost"
+		onClick={(event: any) => {
+			event.stopPropagation();
+			addNewCategory();
+		}}>Add new category</Button>
+		<DraggableTileList
+		dataList={[...selectedComponent.items]}
+		setDataList={updateStepList}
+		updateItem={updateListItems}
+		defaultObject={{
+			text: 'New option',
+			value: 'new-option',
+			disabled: false,
+			hidden: false
+		}}
+		template={template} />
 	</>;
 };
 
@@ -35,7 +223,6 @@ export const ASelectCodeUI = ({ selectedComponent, setComponent }: any) => <Text
 />;
 
 export const ASelect = ({
-	children,
 	componentObj,
 	...rest
 }: any) => {
@@ -66,8 +253,7 @@ export const ASelect = ({
 							value={child.value}
 							disabled={child.disabled}
 							hidden={child.hidden}
-							key={index}>
-						</SelectItem>)
+							key={index} />)
 					}
 					</SelectItemGroup>
 						:
@@ -76,8 +262,7 @@ export const ASelect = ({
 						value={step.value}
 						disabled={step.disabled}
 						hidden={step.hidden}
-						key={index}>
-					</SelectItem>
+						key={index} />
 				)
 			}
 			</Select>
@@ -89,13 +274,13 @@ export const componentInfo: ComponentInfo = {
 	component: ASelect,
 	settingsUI: ASelectSettingsUI,
 	codeUI: ASelectCodeUI,
-	render: ({ componentObj, select, remove, selected, renderComponents, outline }) => <ASelect
-	componentObj={componentObj}
-	select={select}
-	remove={remove}
-	selected={selected}>
+	render: ({ componentObj, select, remove, selected }) => <ASelect
+		componentObj={componentObj}
+		select={select}
+		remove={remove}
+		selected={selected}>
 	</ASelect>,
-	keywords: ['content', 'switcher'],
+	keywords: ['select'],
 	name: 'Select',
 	type: 'select',
 	defaultComponentObj: {
@@ -103,55 +288,28 @@ export const componentInfo: ComponentInfo = {
 		inline: false,
 		invalid: false,
 		disabled: false,
-		labelText: "Select",
-		invalidText: "A valid value is required",
-		defaultValue: "placeholder-item",
-		helperText: "Optional helper text",
+		labelText: 'Select',
+		invalidText: 'A valid value is required',
+		defaultValue: 'placeholder-item',
+		helperText: 'Optional helper text',
 		items: [
 			{
-				text: 'Choose an option 1',
+				text: 'Choose an option',
 				value: 'placeholder-item',
 				hidden: false,
 				disabled: false,
 				type: 'select-item'
 			},
 			{
-				text: 'Choose an option 2',
-				value: 'placeholder-item',
-				hidden: false,
-				disabled: false,
-				type: 'select-item'
-			},
-			{
-				label: "Category 1",
+				label: 'Category 1',
 				disabled: false,
 				items: [
 					{
-						text: "Choose an option 1",
-						value: "placeholder-item",
+						text: 'Choose an option',
+						value: 'placeholder-item',
 						disabled: false,
-						type: 'select-item',
-						hidden: false
-					},
-					{
-						text: "Choose an option 2",
-						value: "placeholder-item",
-						disabled: false,
-						type: 'select-item',
-						hidden: false
-					}
-				]
-			},
-			{
-				label: "Category 2",
-				disabled: false,
-				items: [
-					{
-						text: "Choose an option 1",
-						value: "placeholder-item",
-						disabled: false,
-						type: 'select-item',
-						hidden: false
+						hidden: false,
+						type: 'select-item'
 					}
 				]
 			}
@@ -161,16 +319,49 @@ export const componentInfo: ComponentInfo = {
 	codeExport: {
 		angular: {
 			inputs: (_) => '',
-			outputs: ({ json }) => ``,
+			outputs: ({ json }) => '',
 			imports: [''],
 			code: ({ json }) => {
 				return ``;
 			}
 		},
 		react: {
-			imports: [''],
+			imports: ['Select', 'SelectItem', 'SelectItemGroup'],
 			code: ({ json }) => {
-				return ``;
+				return `<Select
+					id="select"
+					defaultValue="${json.defaultValue}"
+					helperText="${json.helperText}"
+					invalidText="${json.invalidText}"
+					labelText="${json.labelText}"
+					inline={${json.inline}}
+					invalid={${json.invalid}}
+					disabled={${json.disabled}}
+					${reactClassNamesFromComponentObj(json)}>
+					${json.items.map((step: any, index: any) =>
+						step.items && step.items.length > 0
+							?
+						`<SelectItemGroup
+						key={${index}}
+						label="${step.label}"
+						disabled={${step.disabled}}>
+						${step.items.map((child: any, index: any) => `<SelectItem
+							text="${child.text}"
+							value="${child.value}"
+							disabled={${child.disabled}}
+							hidden={${child.hidden}}
+							key={${index}} />`
+						).join('\n')}
+						</SelectItemGroup>`
+							:
+						`<SelectItem
+							text="${step.text}"
+							value="${step.value}"
+							disabled={${step.disabled}}
+							hidden={${step.hidden}}
+							key={${index}} />`
+					).join('\n')}
+				</Select>`;
 			}
 		}
 	}
