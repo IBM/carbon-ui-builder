e253beccf21e0cf02070af9b3a9d411b24e986e7
Connor-Ahearn <connor.ahearn@ibm.com>
2023-01-18 16:14:52 -0500

    Actions UI: Created basic UI for button actions

--- src/fragment-components/a-button.tsx
+++ src/fragment-components/a-button.tsx
@@ -63,19 +63,82 @@ export const AButtonSettingsUI = ({ selectedComponent, setComponent }: any) => {
 };
 
 export const AButtonCodeUI = ({ selectedComponent, setComponent }: any) => {
-	return <TextInput
-			value={selectedComponent.codeContext?.name}
-			labelText='Input name'
-			onChange={(event: any) => {
-				setComponent({
-					...selectedComponent,
-					codeContext: {
-						...selectedComponent.codeContext,
-						name: event.currentTarget.value
+
+	const actionChange = (data: { target: { name: string; value: any; }; }) => {
+		console.log('CONNOR selectedComponent:', selectedComponent);
+		console.log('CONNOR setComponent:', setComponent);
+		console.log('CONNOR data', data);
+	}
+
+	// Should be dynamic
+	// Should depend on the target element
+	// Should only list the compatible slot actions
+	const slotDropdownItems: {text: string}[] = [
+		{ text: "Toggle Disable" },
+		{ text: "Toggle Visibility" }
+	]; 
+
+	// Should be dynamic
+	// Should give a list of elements in current app that can be used as slots
+	const elementDropdownItems: {text: string}[] = [
+		{ text: "Button" }
+	];
+
+	return (
+		<>
+			<TextInput
+				value={selectedComponent.codeContext?.name}
+				labelText='Input name'
+				onChange={(event: any) => {
+					setComponent({
+						...selectedComponent,
+						codeContext: {
+							...selectedComponent.codeContext,
+							name: event.currentTarget.value
+						}
+					});
+				}}
+			/>
+			<div style={{border: '2px #525252 solid', padding: '10px', margin: '5px'}}>
+				<h6 style={{color: '#323232', marginBottom: '5px', fontWeight: 'normal', textDecoration: 'underline'}}>On Click</h6>
+				<Dropdown
+					id="elementDropdown"
+					titleText="Element"
+					helperText="Your existing elements"
+					label=""
+					items={elementDropdownItems}
+					itemToString={(item: any) => (item ? item.text : "")}
+					onChange={(selectedItem: any) =>
+						actionChange({
+							target: {
+								name: "elementDropdown",
+								value: selectedItem
+							}
+						})
 					}
-				});
-			}}
-		/>;
+				/>
+				<Dropdown
+					id="slotDropdown"
+					titleText="Slot"
+					helperText="What changes on the element"
+					label=""
+					items={slotDropdownItems}
+					itemToString={(item: any) => (item ? item.text : "")}
+					onChange={(selectedItem: any) =>
+						actionChange({
+							target: {
+								name: "slotDropdown",
+								value: selectedItem
+							}
+						})
+					}
+				/>
+				<Button kind="ghost" className="title">
+					+
+				</Button>
+			</div>
+		</>
+	);
 };
 
 export const AButton = ({
