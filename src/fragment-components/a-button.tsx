aa2ce3809f514533e1423ca9724f14c0178d7fe6
Connor-Ahearn <connor.ahearn@ibm.com>
2023-01-19 11:54:10 -0500

    Actions UI: Brought fragment data into code UI object, separate actions editor component
    Signed-off-by: Connor-Ahearn <connor.ahearn@ibm.com>

--- src/fragment-components/a-button.tsx
+++ src/fragment-components/a-button.tsx
@@ -9,6 +9,8 @@ import { AComponent, ComponentInfo } from './a-component';
 
 import image from './../assets/component-icons/button.svg';
 import { angularClassNamesFromComponentObj, nameStringToVariableString, reactClassNamesFromComponentObj } from '../utils/fragment-tools';
+import { ActionsEditor } from '../routes/edit/actions';
+import { time } from 'console';
 
 export const AButtonSettingsUI = ({ selectedComponent, setComponent }: any) => {
 	const kindItems = [
@@ -62,26 +64,14 @@ export const AButtonSettingsUI = ({ selectedComponent, setComponent }: any) => {
 	</>;
 };
 
-export const AButtonCodeUI = ({ selectedComponent, setComponent }: any) => {
+export const AButtonCodeUI = ({ selectedComponent, setComponent, fragment }: any) => {
 
-	const actionChange = (data: { target: { name: string; value: any; }; }) => {
-		console.log('CONNOR selectedComponent:', selectedComponent);
-		console.log('CONNOR setComponent:', setComponent);
-		console.log('CONNOR data', data);
-	}
+	console.log('CONNOR fragment #2', fragment);
 
-	// Should be dynamic
-	// Should depend on the target element
-	// Should only list the compatible slot actions
-	const slotDropdownItems: {text: string}[] = [
-		{ text: "Toggle Disable" },
-		{ text: "Toggle Visibility" }
-	]; 
-
-	// Should be dynamic
-	// Should give a list of elements in current app that can be used as slots
-	const elementDropdownItems: {text: string}[] = [
-		{ text: "Button" }
+	// Default value
+	const signals: {text: string; value: string}[] = [
+		{ text: 'On Click', value: 'onclick'},
+		{ text: 'On Hover', value: 'onhover'}
 	];
 
 	return (
@@ -99,44 +89,9 @@ export const AButtonCodeUI = ({ selectedComponent, setComponent }: any) => {
 					});
 				}}
 			/>
-			<div style={{border: '2px #525252 solid', padding: '10px', margin: '5px'}}>
-				<h6 style={{color: '#323232', marginBottom: '5px', fontWeight: 'normal', textDecoration: 'underline'}}>On Click</h6>
-				<Dropdown
-					id="elementDropdown"
-					titleText="Element"
-					helperText="Your existing elements"
-					label=""
-					items={elementDropdownItems}
-					itemToString={(item: any) => (item ? item.text : "")}
-					onChange={(selectedItem: any) =>
-						actionChange({
-							target: {
-								name: "elementDropdown",
-								value: selectedItem
-							}
-						})
-					}
-				/>
-				<Dropdown
-					id="slotDropdown"
-					titleText="Slot"
-					helperText="What changes on the element"
-					label=""
-					items={slotDropdownItems}
-					itemToString={(item: any) => (item ? item.text : "")}
-					onChange={(selectedItem: any) =>
-						actionChange({
-							target: {
-								name: "slotDropdown",
-								value: selectedItem
-							}
-						})
-					}
-				/>
-				<Button kind="ghost" className="title">
-					+
-				</Button>
-			</div>
+			{ signals.map((item) => {
+				return <ActionsEditor text={item.text} value={item.value} />;
+			})}
 		</>
 	);
 };
