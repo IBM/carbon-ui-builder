2fdd97567e52cbb9131e45fb5cc34e25d5111598
Connor-Ahearn <connor.ahearn@ibm.com>
2023-02-02 12:31:42 -0500

    Actions UI: Zvonimir code review changes & working with fragment

--- src/routes/edit/actions.tsx
+++ src/routes/edit/actions.tsx
@@ -9,29 +9,26 @@ import { isPropertySignature } from 'typescript';
 import { allComponents } from '../../fragment-components/index'
 
 // TODO: Come up with a new name for file & Component
-export const ActionsEditor = ({ signal, addAction }: any) => {
+export const ActionsEditor = ({ action, addAction }: any) => {
     const [fragment, setFragment] = useFragment();
 
     //*******************
     // Elements Dropdown
     //*******************
 
-    // ***************
-    // TODO: Use allComponents instead of the below array
-    // ***************
-
+    const actionSupportedElementTypes: string[] = 
+        Object.values(allComponents).filter(component => component.componentInfo.signals)
+        .map(component => component.componentInfo.type);
+        
+    
     /**
-     * Element types that are supported in actions
+     * Uses searchForActionableElements to get elements that contain actions
      * 
-     * 
+     * @returns Array of actionable element names
      */
-    const actionSupportedElementTypes: string[] = [
-		'button'
-	];
+    const getActionableElements = (): string[] => searchForActionableElements([], fragment.data.items);
 
     /**
-     * ***Should use filter***
-     * ***Create helper function***
      * 
      * Recursive function that pulls all the elements in the fragment that are compatible with actions
      * 
@@ -41,14 +38,15 @@ export const ActionsEditor = ({ signal, addAction }: any) => {
      * @param items Recursive data structure, each item may have an array of items depending on the fragment design
      * @returns final value of actionableElements, all the codeContext.name values for elements of our fragment that support actions
      */
-	const getActionableElements = (actionableElements: string[], items: any): string[] => {
-		for(let i = 0; i < items.length; i++) {
+	const searchForActionableElements = (actionableElements: string[], items: any): string[] => {
+        
+        for(let i = 0; i < items.length; i++) {
 			if(actionSupportedElementTypes.includes(items[i].type)) {
 				actionableElements.push(items[i].codeContext.name);
 			}
 
 			if(items[i].items) {
-				getActionableElements(actionableElements, items[i].items);
+				searchForActionableElements(actionableElements, items[i].items);
 			}
 		}
 
@@ -56,7 +54,7 @@ export const ActionsEditor = ({ signal, addAction }: any) => {
 
 	} 
 
-	const actionableElements = getActionableElements([], fragment.data.items);
+	const actionableElements = getActionableElements();
     const elementDropdownItems = actionableElements.map(element => ({ text: element }));
 
 
@@ -75,7 +73,7 @@ export const ActionsEditor = ({ signal, addAction }: any) => {
     ];    
     
     return <div className={css`border: 2px #525252 solid; padding: 10px; margin: 5px;`}>
-        <h6 className={css`color: #323232, marginBottom: 5px; fontWeight: normal; textDecoration: underline;`}>{signal.text}</h6>
+        <h6 className={css`color: #323232, marginBottom: 5px; fontWeight: normal; textDecoration: underline;`}>{action.text}</h6>
         <Dropdown
             id='elementDropdown'
             titleText='Element'
@@ -84,7 +82,7 @@ export const ActionsEditor = ({ signal, addAction }: any) => {
             items={elementDropdownItems}
             itemToString={(item: any) => (item ? item.text : '')}
             onChange={(selectedItem: any) => {
-                signal.destination = selectedItem
+                action.destination = selectedItem
             }}
         />
         <Dropdown
@@ -95,10 +93,10 @@ export const ActionsEditor = ({ signal, addAction }: any) => {
             items={slotDropdownItems}
             itemToString={(item: any) => (item ? item.text : '')}
             onChange={(selectedItem: any) => {
-                signal.slot = selectedItem
+                action.slot = selectedItem
             }}
         />
-        <Button kind='ghost' onClick={() => addAction(signal.text, signal.source, signal.signal)}>
+        <Button kind='ghost' onClick={() => addAction(action.text, action.source, action.signal)}>
             +
         </Button>
     </div>
