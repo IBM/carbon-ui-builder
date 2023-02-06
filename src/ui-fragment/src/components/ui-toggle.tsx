e5c44013073034635e7bdc09a5bffea37072703f
Moiz Masud <moizkhan_007@hotmail.com>
2023-02-06 07:17:18 -0800

    Add empty space back in
    Signed-off-by: Moiz Masud <moizkhan_007@hotmail.com>

--- src/ui-fragment/src/components/ui-toggle.tsx
+++ src/ui-fragment/src/components/ui-toggle.tsx
@@ -25,6 +25,7 @@ export const UIToggle = ({ state, setState }: {
 		// eslint-disable-next-line react/jsx-no-useless-fragment
 		return <></>;
 	}
+
 	return <Toggle
 		labelA={state.offText}
 		labelB={state.onText}
