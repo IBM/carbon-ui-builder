6da52c1b5d3bc22a09f6446bc74938b1e0665023
Dan Stingaciu <dan.stingaciu@ibm.com>
2023-02-24 16:15:30 -0500

    fragment is now only updated with actions when the action is fully filled in
    Signed-off-by: Dan Stingaciu <dan.stingaciu@ibm.com>

--- src/fragment-components/a-button.tsx
+++ src/fragment-components/a-button.tsx
@@ -1,18 +1,20 @@
+import { AComponent, ComponentInfo } from './a-component';
+/* eslint-disable react/react-in-jsx-scope */
 import {
 	Button,
 	Dropdown,
 	TextInput
 } from 'carbon-components-react';
-import { css } from 'emotion';
-import { AComponent, ComponentInfo } from './a-component';
-
-import image from './../assets/component-icons/button.svg';
 import { angularClassNamesFromComponentObj, nameStringToVariableString, reactClassNamesFromComponentObj } from '../utils/fragment-tools';
+
 import { ActionsPane } from '../routes/edit/actions';
-import { time } from 'console';
+import { css } from 'emotion';
+import image from './../assets/component-icons/button.svg';
+// import { time } from 'console';
 import { useFragment } from '../context';
-import { idText } from 'typescript';
-import { useState } from 'react';
+
+// import { idText } from 'typescript';
+// import { useState } from 'react';
 
 export const AButtonSettingsUI = ({ selectedComponent, setComponent }: any) => {
 	const kindItems = [
@@ -70,7 +72,7 @@ export const AButtonCodeUI = ({ selectedComponent, setComponent }: any) => {
 	const [fragment, setFragment] = useFragment();
 
 	// Add default action for this component if its not already present in fragment
-	if(fragment.data.actions.every((action: any) => 
+/* 	if (fragment.data.actions && fragment.data.actions.every((action: any) =>
 		action.source + action.signal + action.id !== selectedComponent.codeContext.name + 'onclick' + 1)) {
 		setFragment({ ...fragment, data: {
 			...fragment.data, actions: [
@@ -87,8 +89,8 @@ export const AButtonCodeUI = ({ selectedComponent, setComponent }: any) => {
 			]
 		} });
 	}
-	
-	const addAction = (text: string, source: string, signal: string) => {
+ */
+	const addAction = (_text: string, _source: string, _signal: string) => {
 		let id = 0;
 		fragment.data.actions.forEach((action: any) => id = (action.id >= id) ? id = action.id + 1 : id);
 
@@ -123,8 +125,9 @@ export const AButtonCodeUI = ({ selectedComponent, setComponent }: any) => {
 					});
 				}}
 			/>
-			{ fragment.data.actions.map((item: any) => {
-				return <ActionsPane action={item} addAction={addAction} key={item.id} />;
+			<ActionsPane addAction={addAction} />
+			{ fragment.data.actions && fragment.data.actions.map((item: any) => {
+				return ;
 				// if (item.source === selectedComponent.codeContext.id) {
 				// 	return <ActionsPane action={item} addAction={addAction} key={item.id} />;
 				// } else {
--- src/routes/edit/actions.tsx
+++ src/routes/edit/actions.tsx
@@ -1,17 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { DraggableTileList } from '../../components';
+/* eslint-disable react/react-in-jsx-scope */
+// import React, { useEffect, useState } from 'react';
 import {
-	Button,
 	Dropdown
 } from 'carbon-components-react';
+// import { isPropertySignature } from 'typescript';
+import { allComponents } from '../../fragment-components/index';
 import { css } from 'emotion';
 import { useFragment } from '../../context';
-import { isPropertySignature } from 'typescript';
-import { allComponents } from '../../fragment-components/index';
-import { DraggableTileList } from '../../components';
+
+interface ActionProps {
+	text: String;
+	source: String;
+	signal: String;
+	destination: String;
+	slot: String;
+	id: Number;
+}
 
 // TODO: Come up with a new name for file & Component
 export const ActionsPane = ({ addAction }: any) => {
 	const [fragment, setFragment] = useFragment();
+
+	const [actionState, setActionState] = useState<ActionProps[]>(fragment.data.actions || []);
 
 	//* ******************
 	// Elements Dropdown
@@ -81,32 +94,32 @@ export const ActionsPane = ({ addAction }: any) => {
 	];
 
 	const handleActionUpdate = (action: any, item: any, updateType: String) => {
-		const filteredActions = fragment.data.actions.map((val: any) => {
-			if (updateType === 'actions') {
-				val.destination = action.selectedItem.text;
-			} else if (updateType === 'slots') {
-				val.slot = action.selectedItem.text;
+		const filteredActions = actionState.map(currentAction => {
+			if (currentAction.id === item.id) {
+				if (updateType === 'actions') {
+					currentAction.destination = action.selectedItem.text;
+				} else if (updateType === 'slots') {
+					currentAction.slot = action.selectedItem.text;
+				}
 			}
-			return val;
+			return currentAction;
 		});
-
-		setFragment({
-			...fragment,
-			data: {
-				...fragment.data,
-				actions: filteredActions
-			}
-		});
+		setActionState(filteredActions);
 	};
 
-	const updateActionsList = (newList: any[]) => {
+	useEffect(() => {
+		const completedActions = actionState.filter(currentAction => currentAction.destination !== '' && currentAction.slot !== '');
 		setFragment({
 			...fragment,
 			data: {
 				...fragment.data,
-				actions: newList
+				actions: completedActions
 			}
 		});
+	}, [actionState]);
+
+	const updateActionsList = (newList: any[]) => {
+		setActionState(newList);
 	};
 
 	const template = (item: any, _index: number) => {
@@ -141,7 +154,7 @@ export const ActionsPane = ({ addAction }: any) => {
 
 	return (
     <DraggableTileList
-    dataList={fragment.data.actions}
+    dataList={actionState}
     setDataList={updateActionsList}
     updateItem={handleActionUpdate}
     template={template}
@@ -151,7 +164,7 @@ export const ActionsPane = ({ addAction }: any) => {
 			signal: 'onclick',
 			destination: '',
 			slot: '',
-			id: ''
+			id: actionState.length
 		}}
 	/>
 	);
