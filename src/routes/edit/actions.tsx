851424bc7a7b796d8908f8a59bc9e87de8f5a8a1
Dan Stingaciu <dan.stingaciu@ibm.com>
2023-02-28 14:46:09 -0500

    Added useEffect that listen for source component changes
    Signed-off-by: Connor-Ahearn <connor.ahearn@ibm.com>

--- src/routes/edit/actions.tsx
+++ src/routes/edit/actions.tsx
@@ -102,6 +102,10 @@ export const ActionsPane = ({ addAction, sourceComponent }: any) => {
 		});
 	}, [actionState]);
 
+	useEffect(() => {
+		setActionState(relatedActions);
+	}, [sourceComponent]);
+
 	const updateActionsList = (newList: any[]) => {
 		setActionState(newList);
 	};
