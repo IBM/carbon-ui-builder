26b96023a644985a6b05acea4aed8c519c344d45
Akshat Patel <akshat@live.ca>
2023-11-09 00:26:29 -0500

    chore: include markdown files in assets included in dist
    Signed-off-by: Akshat Patel <akshat@live.ca>

--- apps/carbon-ui-builder/vite.config.js
+++ apps/carbon-ui-builder/vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	assetsInclude: ['**/*.md'],
 	build: {
 		outDir: '../../dist/apps/carbon-ui-builder'
 	},
-	server: {
-		port: 3000,
-		open: true,
-	},
-	plugins: [react()],
 	define: {
 		// Node.js global workaround
 		global: {}
-	}
+	},
+	plugins: [react()],
+	server: {
+		port: 3000,
+		open: true
+	},
 });
