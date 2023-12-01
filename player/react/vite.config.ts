596de579f13ef9ac871273d8d7bc4d06c2a75053
Akshat Patel <38994122+Akshat55@users.noreply.github.com>
2023-12-01 15:08:06 -0500

    fix: add workflow to release package (#292)
    * chore: update build directory names and path
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: lint fix
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: update project source root
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: Set output path for libs in dist/libs/*
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: Add nx release script
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * ci: Add publish ci flow
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: Add SemVer versioning
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: add commintlint
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: add root workspace
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * Support {{children}} in custom components
    
    Signed-off-by: Zvonimir Fras <zvonimir@zvonimirfras.com>
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * Fix export template not working when items empty
    
    Signed-off-by: Zvonimir Fras <zvonimir@zvonimirfras.com>
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: publish without packing
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * revert: file deletion
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: give execute permissions
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: use carbon bot for publishing github releases
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: skip app commit types
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: add pp as an acceptable commit type
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * chore: update secret name
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    * ci: migrate back to workflow
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    
    ---------
    
    Signed-off-by: Akshat Patel <akshat@live.ca>
    Signed-off-by: Zvonimir Fras <zvonimir@zvonimirfras.com>
    Co-authored-by: Zvonimir Fras <zvonimir@zvonimirfras.com>

--- player/react/vite.config.ts
+++ player/react/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 		lib: {
 			// Could also be a dictionary or array of multiple entry points.
 			entry: 'src/index.ts',
-			name: 'player',
+			name: 'player-react',
 			fileName: 'index',
 			// Change this to the formats you want to support.
 			// Don't forget to update your package.json as well.
--- sdk/react/vite.config.ts
+++ sdk/react/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 		lib: {
 			// Could also be a dictionary or array of multiple entry points.
 			entry: 'src/index.ts',
-			name: 'sdk',
+			name: 'sdk-react',
 			fileName: 'index',
 			// Change this to the formats you want to support.
 			// Don't forget to update your package.json as well.
