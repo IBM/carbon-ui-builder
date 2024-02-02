69dd6e86b52cc78ce1fe0d2c82e9c8e503be9392
Zvonimir Fras <zvonimir@zvonimirfras.com>
2024-02-02 18:43:32 -0500

    Move Fragment to SDK
    Signed-off-by: Zvonimir Fras <zvonimir@zvonimirfras.com>

--- sdk/react/src/lib/components/index.ts
+++ sdk/react/src/lib/components/index.ts
@@ -2,3 +2,4 @@ export * from './actions-connector';
 export * from './fragment-layout-widget';
 export * from './fragment-preview';
 export * from './custom-components-collection-editor';
+export * from './fragment';
