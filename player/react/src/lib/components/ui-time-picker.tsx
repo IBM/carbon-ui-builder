f915cd8dba84547a7c2ea41437afb13500a43fb4
Max You <max.you@outlook.com>
2024-02-14 21:40:39 -0500

    slots
    Signed-off-by: Max You <max.you@outlook.com>

--- player/react/src/lib/components/ui-time-picker.tsx
+++ player/react/src/lib/components/ui-time-picker.tsx
@@ -2,6 +2,8 @@ import { TimePickerSelect } from '@carbon/react';
 import { SelectItem } from '@carbon/react';
 import { TimePicker } from '@carbon/react';
 import React from 'react';
+import { commonSlots, slotsDisabled } from '../common-slots';
+import { SendSignal } from '../types';
 
 export interface TimePickerState {
 	id: string | number;
@@ -20,15 +22,59 @@ export interface TimePickerState {
 	items: [];
 }
 
-export const UITimePicker = ({ state }: {
+export const type = 'time-picker';
+
+export const signals = ['valueChange', 'click'];
+
+export const slots = {
+	...commonSlots,
+	...slotsDisabled,
+	invalid: 'boolean',
+	isInvalid: (state: TimePickerState) => ({
+		...state,
+		invalid: true
+	}),
+	isValid: (state: TimePickerState) => ({
+		...state,
+		invalid: false
+	}),
+	toggleIsInvalid: (state: TimePickerState) => ({
+		...state,
+		invalid: !state.invalid
+	}),
+	light: 'boolean',
+	isLight: (state: TimePickerState) => ({
+		...state,
+		light: true
+	}),
+	isDark: (state: TimePickerState) => ({
+		...state,
+		light: false
+	}),
+	toggleIsLight: (state: TimePickerState) => ({
+		...state,
+		light: !state.light
+	}),
+	placeholder: 'string',
+	type: 'string',
+	invalidText: 'string',
+	label: 'string',
+	size: 'string',
+	value: 'string',
+};
+
+
+export const UITimePicker = ({ state, sendSignal }: {
 	state: TimePickerState;
 	setState: (state: any) => void;
 	setGlobalState: (state: any) => void;
+	sendSignal: SendSignal;
 }) => {
 	if (state.type !== 'time-picker') {
 		// eslint-disable-next-line react/jsx-no-useless-fragment
 		return <></>;
 	}
+
 	return <TimePicker
 		id='time-picker'
 		light={state.light}
@@ -37,7 +83,13 @@ export const UITimePicker = ({ state }: {
 		invalidText={state.invalidText}
 		placeholder={state.placeholder}
 		size={state.size}
-		labelText={state.label}>
+		labelText={state.label}
+		onClick={() => {
+			sendSignal(state.id, 'click');
+		}}
+		onChange={(event: any) => {
+			sendSignal(state.id, 'valueChange', [event.value], { ...state, value: event.value });
+		}}>
 		<TimePickerSelect labelText='time-picker-1' id='time-picker-select-1'>
 			<SelectItem value='AM' text='AM' />
 			<SelectItem value='PM' text='PM' />
