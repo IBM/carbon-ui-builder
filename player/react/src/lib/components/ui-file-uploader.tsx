7d6c4e9e92bea935d1eac5ad569ed1ae303f2582
Max You <JianzhongMax.You@ibm.com>
2024-03-06 22:17:15 -0500

    initial commit
    Signed-off-by: Max You <JianzhongMax.You@ibm.com>

---
+++ player/react/src/lib/components/ui-file-uploader.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { css } from 'emotion';
+import { CssClasses } from '../types';
+import { FileUploaderDropContainer } from '@carbon/react';
+import { FileUploader } from '@carbon/react';
+
+const labelDescriptionStyle = css`
+    color: #525252;
+    font-size: .875rem;
+    font-weight: 400;
+    letter-spacing: .16px;
+	line-height: 1.28572;
+	margin-top: 0.5rem;
+    margin-bottom: 1rem;
+`;
+
+const labelTitleStyle = css`
+    color: #161616;
+    font-size: .875rem;
+    font-weight: 600;
+    letter-spacing: .16px;
+    line-height: 1.28572;
+`;
+
+export interface FileUploaderState {
+	type: string;
+	cssClasses?: CssClasses[];
+	buttonKind: string;
+	buttonLabel: string;
+	labelTitle: string;
+	filenameStatus: string;
+	dragAndDroplabelText: string;
+	labelDescription: string;
+	iconDescription: string;
+	size: string;
+	multiple: boolean;
+	disabled: boolean;
+	dragAndDrop: boolean;
+	codeContext?: {
+		name: string;
+	};
+}
+
+export const UIFileUploader = ({ state }: {
+	state: FileUploaderState;
+	setState: (state: any) => void;
+	setGlobalState: (state: any) => void;
+}) => {
+	if (state.type !== 'file-uploader') {
+		// eslint-disable-next-line react/jsx-no-useless-fragment
+		return <></>;
+	}
+
+	return <>
+		{
+			state.dragAndDrop
+				? <>
+		<strong className={labelTitleStyle}>{state.labelTitle}</strong>
+		<p className={labelDescriptionStyle}>
+			{state.labelDescription}
+		</p>
+		<FileUploaderDropContainer
+            accept={[
+            'image/jpeg',
+            'image/png'
+            ]}
+            multiple={state.multiple}
+            disabled={state.disabled}
+            labelText={state.dragAndDroplabelText}
+            tabIndex={0} />
+				</> :
+		<FileUploader
+            accept={[
+                '.jpg',
+                '.png'
+            ]}
+            buttonKind={state.buttonKind}
+            buttonLabel={state.buttonLabel}
+            filenameStatus={state.filenameStatus}
+            iconDescription={state.iconDescription}
+            labelDescription={state.labelDescription}
+            labelTitle={state.labelTitle}
+            multiple={state.multiple}
+            disabled={state.disabled}
+            size={state.size} />
+		};
+	</>;
+};
