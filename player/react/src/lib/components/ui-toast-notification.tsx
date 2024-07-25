06c78df55770b0e787ffe732a4019b01beabbe95
Max You <JianzhongMax.You@ibm.com>
2024-07-25 10:47:53 -0400

    toast notification progress
    Signed-off-by: Max You <JianzhongMax.You@ibm.com>

---
+++ player/react/src/lib/components/ui-toast-notification.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { CssClasses, SendSignal } from '../types';
+import {
+	ToastNotification
+} from '@carbon/react';
+
+import { commonSlots, slotsDisabled } from '../common-slots';
+
+export interface ToastNotificationState {
+	id: string;
+	type: string;
+	lowContrast?: boolean;
+	isHideCloseButton?: boolean;
+	kind?: string;
+	link?: string;
+	subtitle?: string;
+	linkText?: string;
+	title?: string;
+	iconDescription?: string;
+	caption?: string;
+	cssClasses?: CssClasses[];
+}
+
+export const type = 'notification';
+
+export const signals = ['valueChange', 'click'];
+
+export const slots = {
+	...commonSlots,
+	...slotsDisabled,
+	lowContrast: 'boolean',
+	setLowContrast: (state: ToastNotificationState) => ({
+		...state,
+		lowContrast: true
+	}),
+	setHighContrast: (state: ToastNotificationState) => ({
+		...state,
+		lowContrast: false
+	}),
+	toggleContrast: (state: ToastNotificationState) => ({
+		...state,
+		lowContrast: !state.lowContrast
+	}),
+	hideCloseButton: 'boolean',
+	toHideCloseButton: (state: ToastNotificationState) => ({
+		...state,
+		hideCloseButton: true
+	}),
+	toShowCloseButton: (state: ToastNotificationState) => ({
+		...state,
+		hideCloseButton: false
+	}),
+	toggleCloseButton: (state: ToastNotificationState) => ({
+		...state,
+		hideCloseButton: !state.isHideCloseButton
+	}),
+	type: 'string',
+	kind: 'string',
+	link: 'string',
+	subtitle: 'string',
+	linkText: 'string',
+	title: 'string',
+	iconDescription: 'string',
+	caption: 'string'
+};
+
+export const UIToastNotification = ({ state, sendSignal }: {
+	state: ToastNotificationState;
+	setState: (state: any) => void;
+	setGlobalState: (state: any) => void;
+	sendSignal: SendSignal;
+}) => {
+	if (state.type !== 'notification') {
+		// eslint-disable-next-line react/jsx-no-useless-fragment
+		return <></>;
+	}
+
+	return <ToastNotification
+	className={state.cssClasses?.map((cc: any) => cc.id).join(' ')}
+	onClick={() => {
+		sendSignal(state.id, 'click');
+	}}
+	onChange={(event: any) => {
+		sendSignal(state.id, 'valueChange', [event.value], { ...state, value: event.value });
+	}}
+	caption={state.caption}
+	iconDescription={state.iconDescription}
+	hideCloseButton={state.isHideCloseButton}
+	lowContrast={state.lowContrast}
+	kind={state.kind}
+	subtitle={state.subtitle}
+	timeout={0}
+	title={state.title} />;
+};
